7e44d149b786efcecccc5d1451299ae93122252a
Jayanta Ghosh <jayantaghosh822@gmail.com>
2025-10-18 13:58:10 +0530

    Initial commit1

--- controllers/userController.js
+++ controllers/userController.js
@@ -541,11 +541,13 @@ class UserController {
                 httpOnly: true,
                 secure: true,
                 sameSite: "None",
+                domain: "e-commerce-backend-shdg.onrender.com"
             });
             res.clearCookie("refreshToken", {
                 httpOnly: true,
                 secure: true,
                 sameSite: "None",
+                domain: "e-commerce-backend-shdg.onrender.com"
             });
             res.status(200).json({ message: "Logged out" });
         }catch(err){
